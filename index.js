9e05191559c4943f8c611c502680e79570eabc8f
vietluu <74524082+vietluu@users.noreply.github.com>
2024-01-23 22:56:02 +0700

    config project

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-import express from 'express';
+require('dotenv').config()
+const http = require('http')
+const cluster = require('cluster')
+const { cpus } = require('os')
+const process = require('process')
+const app = require('./app')
+const numCPUs = process.env.NODE_ENV === 'production' ? cpus().length : 1
 
-const app = express();
+if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`)
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
 
-app.listen(3000, () => console.log('server is running in port 3000'));
+    cluster.on('exit', (worker) => {
+        console.log(`worker ${worker.process.pid} died`)
+    })
+} else {
+    const HTTP_PORT = normalizePort(process.env.PORT || 8000)
+    app.set('port', HTTP_PORT)
+    http.createServer(app).listen(HTTP_PORT, onListening)
+}
 
+function onListening() {
+    const addr = this.address()
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+    console.log('Web server listening on ' + bind)
+}
+
+function normalizePort(val) {
+    const port = parseInt(val, 10)
+    if (isNaN(port)) return val
+    if (port >= 0) return port
+    return false
+}
